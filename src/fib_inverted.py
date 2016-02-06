57ce17f4c3ea503e4863802dadff36e12966ef1e
matt mcgraw <matthew.mcgraw@gmail.com>
2016-02-06 18:24:41 -0500

    initial commit for inverted fib

---
+++ src/fib_inverted.py
@@ -0,0 +1,31 @@
+import time
+
+def memoizer(cache, n):
+    for number in range(0,n+1):
+        if n not in cache:
+            cache = julia_fib(cache, number)
+    fib = cache[n]
+    return fib
+
+def julia_fib(cache, n):
+    if n not in cache:
+        if n<2:
+            cache[n] = n
+            return cache
+        cache[n] = cache[n-1] + cache[n-2]
+        return cache
+    else:
+        return cache
+
+if __name__ == "__main__":
+    test_n = 35
+    total_time = 0.0
+    for i in range(0,10):
+        cache = {}
+        t = time.time()
+        actual = memoizer(cache, test_n)
+        t = time.time()-t
+        print(t)
+        total_time = total_time + t
+    avg_time = total_time / 10.0
+    print ("Lang:Python3,Result:%s,Runtime:%s" % (actual,avg_time))
