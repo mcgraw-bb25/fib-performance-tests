d92db06c7d8fa524cf9f212d9a1e8ab8613c7dcf
matt mcgraw <matthew.mcgraw@gmail.com>
2016-02-06 18:20:27 -0500

    initial commit for cython extension

---
+++ src/fib_cython3.py
@@ -0,0 +1,14 @@
+from cython_ext import juliafib
+import time
+
+if __name__ == "__main__":
+    test_n = 35
+    total_time = 0.0
+    for i in range(0,10):
+        t = time.time()
+        actual = juliafib.julia_fib(test_n)
+        t = time.time()-t
+        print (t)
+        total_time = total_time + t
+    avg_time = total_time / 10.0
+    print ("Lang:Cython3,Result:%s,Runtime:%s" % (actual,avg_time))
