b06cac6a2de89ac6f03eb718d1f505075ba327d3
matt mcgraw <matthew.mcgraw@gmail.com>
2016-02-06 18:08:08 -0500

    initial commit for ruby

---
+++ src/fib.rb
@@ -0,0 +1,22 @@
+require "time"
+
+def fib(n)
+    if n < 2
+        return n
+    end
+    return fib(n-1) + fib(n-2)
+end
+
+test_n = 35
+total_time = 0.0
+actual = 0
+10.times do
+    t = Time.new.to_f
+    actual = fib(test_n)
+    t = Time.new.to_f - t
+    puts "#{t}"
+    total_time = total_time + t
+end
+avg_time = total_time / 10.0
+puts "Lang:Ruby,Result:#{actual},Runtime:#{avg_time}"
+
