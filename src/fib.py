9707607c089a38068ec9e0ba6a2ecf15d774bd48
matt mcgraw <matthew.mcgraw@gmail.com>
2016-02-06 17:43:33 -0500

    initial commit for native Python

---
+++ src/fib.py
@@ -0,0 +1,18 @@
+import time
+
+def julia_fib(n):
+    if n<2:
+        return n
+    return julia_fib(n-1)+julia_fib(n-2)
+
+if __name__ == "__main__":
+    test_n = 35
+    total_time = 0.0
+    for i in range(0,10):
+        t = time.time()
+        actual = julia_fib(test_n)
+        t = time.time()-t
+        print(t)
+        total_time = total_time + t
+    avg_time = total_time / 10.0
+    print ("Lang:Python3(Native),Result:%s,Runtime:%s" % (actual,avg_time))
