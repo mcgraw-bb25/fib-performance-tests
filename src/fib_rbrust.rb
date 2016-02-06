f3760a0febab4cd128c89e387fbf11b633711b9a
matt mcgraw <matthew.mcgraw@gmail.com>
2016-02-06 18:22:36 -0500

    initial commit for rust and ruby

---
+++ src/fib_rbrust.rb
@@ -0,0 +1,22 @@
+require "time"
+require "ffi"
+
+module Fib
+	extend FFI::Library
+	ffi_lib "rust_ext/libfib_lib.so"
+	attach_function :fib, [ :int ], :int
+end
+
+test_n = 35
+total_time = 0.0
+actual = 0
+10.times do
+    t = Time.new.to_f
+    actual = Fib.fib(test_n)
+    t = Time.new.to_f - t
+    puts "#{t}"
+    total_time = total_time + t
+end
+avg_time = total_time / 10.0
+puts "Lang:rustyRuby,Result:#{actual},Runtime:#{avg_time}"
+
