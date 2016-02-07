e71e59e36b1b38634d101616d461dec6ace664ce
matt mcgraw <matthew.mcgraw@gmail.com>
2016-02-07 17:50:33 -0500

    output and loops change for Golang

--- src/fib.go
+++ src/fib.go
@@ -18,14 +18,16 @@ func main() {
     var result int
     var total_time float64
     var avg_time float64
+    var counter float64
 
-    for i := 0; i < 10; i++ {
+    for i := 0; i < 20; i++ {
         start := time.Now()
         result = fib(test_n)
         stop := time.Now()
         total_time = total_time + float64(stop.Sub(start))/1E9
         // fmt.Printf("%v\n", float64(stop.Sub(start))/1E9)
+        counter = float64(i)
     }
-    avg_time = (total_time/10.0)
-    fmt.Printf("Lang:Go,Results:%v,Runtime:%v\n",result,avg_time)
+    avg_time = (total_time/(counter+1.0))
+    fmt.Printf("Lang:Go,Results:%v,TotalTime:%v,Loops:%v,Runtime:%v\n",result,total_time,counter+1,avg_time)
 }
