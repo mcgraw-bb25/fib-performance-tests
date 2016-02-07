86dc0b3812650edc18fc4f387c8a8e6f7ed7c90e
matt mcgraw <matthew.mcgraw@gmail.com>
2016-02-06 19:39:30 -0500

    initial commit for test runner

---
+++ src/run_tests.sh
@@ -0,0 +1,31 @@
+set -e
+
+output_file=test_output
+
+case $1 in
+  "core")
+    if [ -e $output_file ]
+    then
+        rm $output_file
+    fi
+
+    clang-3.5 -o fib_c_out.out fib.c
+    ./fib_c_out.out >> $output_file
+
+    javac fib.java
+    java fib >> $output_file
+
+    julia fib.jl >> $output_file
+
+    go run fib.go >> $output_file
+
+    python3 fib.py >> $output_file
+
+    ruby fib.rb >> $output_file;;
+
+  "extend")
+    python3 fib_cython3.py >> $output_file
+    python3 fib_pyrust.py >> $output_file
+    python3 fib_inverted.py >> $output_file
+    ruby fib_rbrust.rb >> $output_file;;
+esac
